90cac6fac60d52e5347323b927d03e84f963e07f
unknown <insta.cclicks@gmail.com>
2025-08-01 21:43:08 +0200

    analytics

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ html.menu = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VB0J9E5KW7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VB0J9E5KW7');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="/">
