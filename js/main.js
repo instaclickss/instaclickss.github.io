ef23dd8c660e47aee34d2a635a158986ffc87277
unknown <emra7331@gmail.com>
2025-09-14 20:41:19 +0200

    mobile fix

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ html.menu = `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="/">
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
     <link rel="stylesheet" href="css/menu.css">
